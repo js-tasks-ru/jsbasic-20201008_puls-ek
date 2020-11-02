b99c0e6c7ae5c9791086dc4a0d8bfb42624b18db
Alexandr Korovaichenko <puls.ek@rambler.ru>
2020-11-02 05:19:08 +0300

    задача 3 из 5 модуля

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,93 @@
+
 function initCarousel() {
-  // ваш код...
-}
+  
+	  let Container, 
+		  Carousel,
+		  arrowLeft,
+		  arrowRight;
+	  
+		// элементы с которыми работаем
+		container  = document.body.querySelector(".carousel");	
+		carousel   = document.body.querySelector(".carousel__inner");
+		massSlides = document.body.querySelectorAll(".carousel__slide");
+		arrowLeft  = document.body.querySelector(".carousel__arrow.carousel__arrow_left");	
+		arrowRight = document.body.querySelector(".carousel__arrow.carousel__arrow_right");	
+		
+		// сразу скрываем стредку вправо
+		arrowLeft.style.display = 'none';
+		
+		let processor = {};
+		processor.arrowLeft 	 = arrowLeft; // DIV влево
+		processor.arrowRight 	 = arrowRight; // DIV вправо
+		processor.carousel	  	 = carousel; // DIV carousel	
+		//processor.carouselLeft   = 0;	
+		//processor.slideWidth 	 = 0;
+		processor.slide 	     = massSlides[0];
+		processor.qtySlides		 = massSlides.length;
+		processor.current  		 = 1;
+		
+		processor.move = function (direction, arrow){
+							
+				if(this.current == 1) {this.arrowLeft.style.display = '';}
+				if(this.current == 4) {this.arrowRight.style.display = '';}
+				this.slideWidth = this.slide.offsetWidth; 
+				this.carouselLeft = this.carousel.offsetLeft;
+				
+				if(direction == "right" && this.current == this.qtySlides-1){
+					//передвигаем на последний слайдер и скрываем кнопку
+					this.current = this.qtySlides;
+					//this.carouselLeft = this.carousel.offsetLeft - this.containerWidth;
+					this.carouselLeft = this.carouselLeft - this.slideWidth;
+					this.carousel.style.transform = "translateX(" + String(this.carouselLeft) + "px)";
+					arrow.style.display = 'none';
+					return;
+				}
+				
+				if(direction == "right" && this.current < this.qtySlides){
+					//cдвигаем влево
+					this.current = this.current + 1;
+					this.carouselLeft = this.carouselLeft - this.slideWidth;
+					this.carousel.style.transform = "translateX(" + String(this.carouselLeft) + "px)";
+					arrow.style.display = '';
+					return;
+				}	
+				
+				if(direction == "left" && this.current == 2){
+					//передвигаем на последний слайдер и скрываем кнопку
+					this.current = 1;
+					this.carouselLeft = this.carouselLeft + this.slideWidth;
+					this.carousel.style.transform = "translateX(" + String(this.carouselLeft) + "px)";
+					arrow.style.display = 'none';
+					return;
+				}				
+				
+				if(direction == "left" && this.current > 1){
+					//cдвигаем вправо
+					this.current = this.current - 1;
+					this.carouselLeft = this.carouselLeft + this.slideWidth;
+					this.carousel.style.transform = "translateX(" + String(this.carouselLeft) + "px)";
+					arrow.style.display = '';
+					return;
+				}	
+			
+			}
+		
+		// устнавливаем обработчики на стрелки
+		let massArrows = document.body.querySelectorAll(".carousel__arrow");
+		for(let i=0; i<massArrows.length; i++){massArrows[i].onclick = elemKlick;}
+		
+		function elemKlick(event){
+				
+				let elemTarget = event.target;
+				
+				// клик на картинке или на диве слева
+				if(elemTarget.classList.contains("carousel__arrow_left") || elemTarget.parentElement.classList.item(1) == "carousel__arrow_left"){ 
+						processor.move("left", this);
+					}
+					
+				// клик на картинке или на диве	справа
+				if(elemTarget.classList.contains("carousel__arrow_right") || elemTarget.parentElement.classList.item(1) == "carousel__arrow_right"){
+						processor.move("right", this);
+					}
+				}
+			}
